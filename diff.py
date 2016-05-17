57042e3c3fba4a472c09d13943abacce15c472f1
Wen Zhaoduo <wenzhaoduo@xiaomi.com>
2016-05-17 11:04:11 +0800

    add version and branch

--- diff.py
+++ diff.py
@@ -80,19 +80,28 @@ def get_pom_base(project_dir, branch = "master", version = None):
     proc = subprocess.check_call(["mv", "pom_newversion.xml", "pom.xml"])
 
 
+#TODO: 如果指定branch和version，该如何调用
+#TODO: 查清楚，不同branch下的不同commit，有没有可能具有相同的commitID
 def main():
     parser = argparse.ArgumentParser()
     
     parser.add_argument("newpompath", help = "the path of pom.xml")
 
     parser.add_argument("-b", "--branch", help = "select a branch. Default branch is master, if version number is not specified")
-    parser.add_argument("-v", "--version", help = "select a version number. If it is given, do not specify a branch.")
+    parser.add_argument("-v", "--version", help = "select a version number. This will ignore the branch if it is given")
 
     args = parser.parse_args()
 
     project_dir = os.path.dirname(args.newpompath)
 
-    get_pom_base(project_dir)
+    if args.version:
+        get_pom_base(project_dir, version = args.version)
+
+    elif args.branch:
+        get_pom_base(project_dir, branch = args.branch)
+
+    else:
+        get_pom_base(project_dir)
 
     find_diff(project_dir)
 
